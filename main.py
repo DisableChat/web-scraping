83da809be32573fa90077e2430d07e4f67fad0f5
MagikarpUsedFly <wr140815@ohio.edu>
2018-08-22 16:33:27 -0400

    main script

---
+++ main.py
@@ -0,0 +1,7 @@
+# Creator: MagikarpUsedFly
+
+##
+# The purpose of this script is to dabble in the BeautifulSoup.
+# Where the main outline/objective is to webscrape the sight newegg for info
+# for some hardware.
+##
